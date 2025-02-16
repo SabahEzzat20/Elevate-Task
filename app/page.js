3476301d371e41b52259dcc2b4e088dfa59f5ee5
Sabah Hassan <sabahezzat20@gmail.com>
2025-02-16 20:05:29 +0200

    metadata rename

--- app/page.js
+++ app/page.js
@@ -11,7 +11,6 @@ export default async function Home() {
   
   return (
     <>
-      
       <Grid container spacing={5} justifyContent='center' size={12} className={styles.productsCatalog}>
         {data.map((product) => (
           <Grid size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 3}} key={product.id} className={styles.product}>
